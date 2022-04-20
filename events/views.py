e5f39b84a7e14260d222b2e2ff6fe0c3dfeddb52
Honza <chudicekjan@gmail.com>
2022-04-20 16:08:38 +0200

    include into main urls.py

--- events/views.py
+++ events/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from models import Event
 
-# Create your views here.
+
+class EventsListView(ListView):
+    model = Event
+    # queryset = Event.objects.all().order_by('-date')
+    template_name = "events.html"
