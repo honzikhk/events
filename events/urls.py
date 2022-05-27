97ac6d20786a106e1aea82ba4ae61fad26482b89
Honza <chudicekjan@gmail.com>
2022-05-27 12:05:08 +0200

    last commit was long time ago

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from events.views import CreateEventView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-urlpatterns = (
+urlpatterns = [
     path("", CreateEventView.as_view(), name="create_event"),
-)
+]
+
+urlpatterns += staticfiles_urlpatterns()
+
